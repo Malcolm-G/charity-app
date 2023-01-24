15db6d860f320a2d16f705e820db7af341daf849
Osorabrian <osorabrian@gmail.com>
2023-01-24 14:05:52 +0300

    format the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from "react"
 
-export default function CardComponent(){
+export default function CardComponent({charityName, category, city, state}){
+    
     return (
-        <div>
-            
+        <div className="card col-3 ms-2 mt-2" >
+            <div className="card-body">
+                <h5>{charityName}</h5>
+                <h6>{category}</h6>
+                <p>{city}, {state}</p>
+                <button className="btn btn-primary col-6 me-2">Show Details</button>
+                <button className="btn btn-success col-4">$ Donate</button>
+            </div>
         </div>
     )
 }
--- src/components/Finder.js
+++ src/components/Finder.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Finder(){
 
-    return
+    return(
+        <div>
+            
+        </div>
+    )
 }
 
 export default Finder;
