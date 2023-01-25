45240f1c81683a52cba3ee1821343a5d83cadc10
Lorraineken <lorrainekupa@gmail.com>
2023-01-25 12:36:47 +0300

    Added sample charity list and description

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
 import React from "react";
 import '../stylesheets/home.css'
+import CardComponent from "./Card";
 
-function Home(){
+function Home({charityList}){
 
+    const charityCard = charityList.map((charity,index) => {
+       if (index<=10){return (
+        <CardComponent 
+        key={charity.ein}
+        charityName={charity.charityName} 
+        category={charity.category} 
+        city={charity.city}
+        state={charity.state}
+        />
+        )}
+        })
   
     return(
         <div>
@@ -11,10 +23,15 @@ function Home(){
           <p>Come and make them Smile</p>
           </header>
           <div className="home-pic">
-            <h3>Welcome !!</h3>
+            <h3>WELCOME !!</h3>
             <p>We are your trusted patners in support of charity and charity organizations </p>
             <p>Touch A Life is a non-profit organization that represents charity organizations which have undergone a series of screening to ensure they meet the highest standards of public accountability and effectiveness. </p>
             <p>We guarantee every patner charity included is financially competent and is a non-profit organization </p>
+            <h4>Some of our enlisted charity organizations </h4>
+            <div id='home-list' className="d-flex" >
+                
+             {charityCard}
+            </div>
           </div>
 
         </div>
