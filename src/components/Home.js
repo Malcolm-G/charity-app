7766d765e2a60babe5aa8e530cee00b9cfedd7f1
Lorraineken <lorrainekupa@gmail.com>
2023-01-25 11:04:42 +0300

    Added home page design and about

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
+import '../stylesheets/home.css'
 
 function Home(){
 
   
     return(
         <div>
+            <header>
           <h1>TOUCH A LIFE</h1>
+          <p>Come and make them Smile</p>
+          </header>
+          <div className="home-pic">
+            <h3>Welcome !!</h3>
+            <p>We are your trusted patners in support of charity and charity organizations </p>
+            <p>Touch A Life is a non-profit organization that represents charity organizations which have undergone a series of screening to ensure they meet the highest standards of public accountability and effectiveness. </p>
+            <p>We guarantee every patner charity included is financially competent and is a non-profit organization </p>
+          </div>
 
         </div>
     )
