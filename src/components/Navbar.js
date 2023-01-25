20006af044da301aa55519497cabb25128d8cc35
Malcolm-G <malcolm.githuka@gmail.com>
2023-01-25 22:38:44 +0300

    Added Login to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"
+                    <Link className="navbar-brand h1" to="/"
                     >Charity-App</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -14,14 +14,11 @@ function Navbar(){
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/finder">CHARITY-FINDER</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/login">LOGIN</Link>
+                                <Link className="nav-link " aria-current="page" to="/finder">CHARITY-FINDER</Link>
                             </li>
                         </ul>
-                        <Link className="nav-link active" aria-current="page" to="/login">LOGIN</Link>
                     </div>
+                    <Link className="nav-link navbar-brand" to="/login" >LOGIN</Link>
                 </div>
             </nav>
     )
