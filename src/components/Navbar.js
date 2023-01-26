1bfa30a0ecd3f32d9c7a91a52a26c4cb76cd832a
Malcolm-G <malcolm.githuka@gmail.com>
2023-01-26 15:09:18 +0300

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,12 @@ function Navbar(){
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0 w-50">
                             <li className="nav-item">
-                                <Link className="nav-link " aria-current="page" to="/finder">CHARITY-FINDER</Link>
+                                <Link className="nav-link mx-5" aria-current="page" to="/finder">CHARITY-FINDER</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-5" aria-current="page" to="/donations">MY-DONATIONS</Link>
                             </li>
                         </ul>
                     </div>
